3170bf5334339e2fd53276d601e333628d0b3ad2
Geoff Willings <geoff@azexis.com>
2021-12-16 11:55:18 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
 # theme-geolocator-js
-JavaScript code to help implement geotargeting prompts or redirects on Evance themes. 
+JavaScript code to help implement geotargeting prompts or redirects on Evance themes
+using the [Evance Geolocation Ajax API](https://www.evance.it/help/themes/ajax/geolocation).
+
+This scripts supports two methods for locale geotargeting:
+1. Suggested locale prompts (recommended) - you've probably seen something similar on large sites like Amazon, or Apple.
+2. Automatic redirects - redirect users to the most appropriate locale.
+
+We recommend using "Suggested locale prompts" because it offers a better user experience.
+
+This script uses a cookie `ev-locale` to store the user's locale preference.
+This is an operational cookie and does not store any personally identifiable data.
+
+## Requirements
+This script requires jQuery and the jQuery.cookie extension. 
+Both of these are included by default into most Evance themes.
+
+## Installation
+Download the `geolocator.js` file or copy the code and
+add it into your Evance Theme's available JavaScript files. 
+Usually these are located in the `~/theme/common/js` directory.
+
+Add the `~/theme/common/js/geolocator.js` file into your list of imported scripts. 
+This is usually `~/theme/common/js/common.json`.
+
+```javascript
+{
+    "files": [
+        
+        // ... existing scripts ...
+            
+        "~/theme/common/js/geolocator.js"
+    ]
+}
+```
+
+### Suggested locale prompts
+Open your default layout for your website, 
+this is usually `~/theme/layouts/index.evml`.
+
+Add the following HTML code just after the `<body>` tag. 
+This will act as a container for the locale suggestion prompt.
+
+```html
+<div id="geolocator-prompt"></div>
+```
+
+Next, add a JavaScript template:
+```html
+<script id="geolocator-template" type="text/x-template">
+    Visit our <%= countryName %> store in <%= currencyCode %>.
+    <a href="<%= url %>">Continue</a>
+    <a href="#dismiss">Stay</a>
+</script>
+```
+This allows control over the message, look and feel of the prompt.
+Where `<%= %>` represents an output tag and `<% %>` represents logic.
+
+Available variables are:
+- `id` - the URL identifier of a locale (e.g. `en-us`)
+- `countryCode` - the locale's ISO-3166 alpa-2 country code in uppercase (e.g. `US`).
+- `countryName` - the country name.
+- `currencyCode` - the ISO-4217 code for the locale's currency in uppercase (e.g. `USD`).
+- `languageCode` - the ISO-639-1 code for the locale' language in lowercase (e.g. `en`)
+- `languageName` - the language name.
+- `timezone` - a timezone string (e.g. `Europe/London`)
+- `url` - the redirection URL to set and save the preferred locale.
+
+Next, add the following to your site's custom JavaScript, 
+either within an existing `.js` file, or a new one.
+
+```javascript
+const geolocator = new Geolocator();
+geolocator.prompt();
+```
+
+### Redirecting users
+Add the following JavaScript code to your site's custom JavaScript.
+Unlike suggestion prompts, this does not require any other code.
+
+```javascript
+const geolocator = new Geolocator();
+geolocator.redirect();
+```
+
+## Configuration
+You may wish to customise what happens when the prompt shows or is dismissed. 
+You do this by supplying `onPrompt` and `onDismiss` functions to the construct.
+
+### onPrompt
+Add your own functionality when a suggested locale prompt is shown.
+```javascript
+const geolocator = new Geolocator({
+    onPrompt: function(locale) {
+        console.log(locale); // The suggested locale object
+        console.log(this); // The container element
+    }
+});
+geolocator.prompt();
+```
+
+### onDismiss
+A user should be allowed to dismiss a suggested locale prompt. 
+By default, this script will look for a `<a href="#dismiss">` link within your container element,
+which will empty the container when clicked. 
+However, you can customise this behaviour
+by adding a `dismiss` selector and/or `onDismiss` method to your config.
+
+```javascript
+const geolocator = new Geolocator({
+    dismiss: '#my-dismiss-button',
+    onDismiss: function() {
+        console.log(this); // The container element
+    }
+});
+geolocator.prompt();
+```
+
+## Minification
+Evance will automatically minify JavaScript included into themes.
+Hence, we have not published a minified version. 
+Simply install and let Evance handle minification for you.
---
+++ geolocator.js
@@ -0,0 +1,157 @@
+/**
+ * # Geolocator
+ *
+ * A JavaScript object designed for geotargeting on Evance website themes.
+ *
+ * @param {Object} options
+ * @return {{redirect: redirect, prompt: prompt}}
+ */
+const Geolocator = function(options) {
+
+    let config = $.extend({
+        cookieName: 'ev-locale',
+        cookieDays: 90,
+        container: '#geolocator-prompt',
+        dismiss: 'a[href="#dismiss"]',
+        template: '#geolocator-template',
+        onPrompt: null,
+        onDismiss: null
+    }, options);
+
+    /**
+     * Returns the current locale code.
+     * e.g. 'en-gb' or NULL if the meta tag cannot be found.
+     */
+    function getCurrentLocaleCode() {
+        let localeMeta = $('meta[name="ev:locale"]');
+        if (!localeMeta.length) {
+            return null;
+        }
+        return localeMeta.attr('content');
+    }
+
+    /**
+     * Returns the current value of the ev-locale
+     * preferred locale cookie, or NULL.
+     */
+    function getPreferredLocaleCode() {
+        return $.cookie(config.cookieName);
+    }
+
+    /**
+     * Send a POST request to the geolocate.json API.
+     * This will return the recommended and preferred locale data.
+     * @return {void}
+     */
+    function geolocate(responseHandler) {
+        // We only need to geolocate if we're not on our preferred locale already.
+        if (isCurrentLocale(getPreferredLocaleCode())) {
+            return;
+        }
+        $.post('/utils/geolocate.json', {
+            url: window.location.pathname
+        }, function (response) {
+            // Does the user have a preferred locale?
+            // If it does not match the current locale
+            // we can ask if they wish to change.
+            const localeOptions = response.data.locale;
+            if (localeOptions.preferred) {
+                responseHandler(localeOptions.preferred);
+            } else if (localeOptions.recommended) {
+                responseHandler(localeOptions.recommended);
+            } else {
+                saveCurrentLocale();
+            }
+        });
+    }
+
+    /**
+     * Handles a specific locale from the API response.
+     * @param {Object} locale
+     */
+    function handleLocalePrompt(locale) {
+        if (!isCurrentLocale(locale.id)) {
+            suggestLocale(locale);
+        } else {
+            saveLocale(locale);
+        }
+    }
+
+    /**
+     * @param {Object} locale
+     */
+    function handleLocaleRedirect(locale) {
+        if (!isCurrentLocale(locale.id)) {
+            saveLocale(locale);
+            window.location.href = locale.url;
+        } else {
+            saveLocale(locale);
+        }
+    }
+
+    /**
+     * Returns true if the localeCode supplied matches the current locale.
+     * @param {string} localeCode
+     * @return {bool}
+     */
+    function isCurrentLocale(localeCode) {
+        return getCurrentLocaleCode() == localeCode;
+    }
+
+    /**
+     * Set the ev-locale cookie ourselves.
+     * This avoids further API interaction.
+     */
+    function saveCurrentLocale() {
+        saveLocale({
+            id: getCurrentLocaleCode()
+        });
+    }
+
+    /**
+     * Save the locale provided as the preferred locale.
+     * This should then prevent any further geolocation attempts.
+     * @param {Object} locale
+     */
+    function saveLocale(locale) {
+        $.cookie(config.cookieName, locale.id, { expires: config.cookieDays, path: '/', secure: true });
+    }
+
+    /**
+     * Suggest a locale to change to.
+     * Requires an empty container element and a template script.
+     * @param {Object} locale
+     */
+    function suggestLocale(locale) {
+        let templateEl = $(config.template);
+        let containerEl = $(config.container);
+        if (!templateEl.length || !containerEl.length) {
+            console.warn('Missing template or container elements for Geolocator');
+        }
+        let template = evance.template(templateEl.html());
+        containerEl.html(template(locale));
+
+        if (config.onPrompt) {
+            config.onPrompt.call(containerEl, locale);
+        }
+
+        $(config.dismiss, containerEl).on('click', function(e) {
+            e.preventDefault();
+            saveCurrentLocale();
+            if (config.onDismiss) {
+                config.onDismiss.call(containerEl);
+            } else {
+                containerEl.empty();
+            }
+        });
+    }
+
+    return {
+        redirect: function() {
+            geolocate(handleLocaleRedirect);
+        },
+        prompt: function() {
+            geolocate(handleLocalePrompt);
+        }
+    };
+};
